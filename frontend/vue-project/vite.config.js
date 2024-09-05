8bc024cd1219790b1033bb90fdc564f0347c0274
김성중 <elecsjkim@gmail.com>
2024-09-05 14:16:53 +0900

    set: SpringBoot & Vue 연동

--- frontend/vue-project/vite.config.js
+++ frontend/vue-project/vite.config.js
@@ -12,5 +12,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build:{
+    outDir: '../../sjkim/src/main/resources/static'
   }
 })
