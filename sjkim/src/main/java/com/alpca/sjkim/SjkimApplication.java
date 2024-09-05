4dc34f4f9f79a073b88fc363a7d8d522310011cf
김성중 <ksj322@tukorea.ac.kr>
2024-09-05 10:29:49 +0900

    init backend && frontend

---
+++ sjkim/src/main/java/com/alpca/sjkim/SjkimApplication.java
@@ -0,0 +1,13 @@
+package com.alpca.sjkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjkimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjkimApplication.class, args);
+	}
+
+}
---
+++ sjkim/src/test/java/com/alpca/sjkim/SjkimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpca.sjkim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjkimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
